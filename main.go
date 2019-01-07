d36b37858f981b418eec55c3b890dffc6545438c
dancing-koala <ccbbruneau@gmail.com>
2019-01-07 21:51:25 +0100

    Implement Hello wold program

--- main.go
+++ main.go
@@ -1,5 +1,54 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 
+	nx := 200
+	ny := 100
+
+	data := getPPMHeader(nx, ny)
+
+	for j := ny - 1; j >= 0; j-- {
+		for i := 0; i < nx; i++ {
+			r := float64(i) / float64(nx)
+			g := float64(j) / float64(ny)
+			b := float64(0.2)
+
+			ir := int(255.99 * r)
+			ig := int(255.99 * g)
+			ib := int(255.99 * b)
+
+			data += fmt.Sprintf("%d %d %d\n", ir, ig, ib)
+		}
+	}
+
+	writeFile([]byte(data))
+}
+
+func getPPMHeader(nx, ny int) string {
+	return fmt.Sprintf("P3\n%d %d\n255\n", nx, ny)
+}
+
+func writeFile(data []byte) {
+	file, err := os.Create("picture.ppm")
+
+	checkErr(err)
+
+	defer file.Close()
+
+	n, err := file.Write(data)
+
+	checkErr(err)
+
+	fmt.Printf("Wrote %d bytes", n)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
