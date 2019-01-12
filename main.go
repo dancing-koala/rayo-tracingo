17fe131174b0afb06a77fd54bb7889e973853a89
dancing-koala <ccbbruneau@gmail.com>
2019-01-12 15:02:14 +0100

    Add sphere struct & use it

--- main.go
+++ main.go
@@ -6,30 +6,19 @@ import (
 	"os"
 )
 
-func hitSphere(center *vec3, radius float64, r *ray) float64 {
-	oc := sub(r.origin(), center)
-	a := dot(r.direction(), r.direction())
-	b := 2.0 * dot(oc, r.direction())
-	c := dot(oc, oc) - radius*radius
-	discriminant := b*b - 4.0*a*c
+func color(r *ray, hitables hitableList) *vec3 {
+	record := &hitRecord{}
 
-	if discriminant < 0 {
-		return -1.0
-	}
-
-	return (-b - math.Sqrt(discriminant)) / (2.0 * a)
-}
-
-func color(r *ray) *vec3 {
-	t := hitSphere(newVec3From(0, 0, -1), 0.5, r)
-
-	if t > 0 {
-		n := unitVector(sub(r.pointAtParam(t), newVec3From(0, 0, -1.0)))
-		return scalarMul(newVec3From(n.x()+1, n.y()+1, n.z()+1), 0.5)
+	if hitables.hit(r, 0.0, math.MaxFloat64, record) {
+		return scalarMul(
+			newVec3From(record.normal.x()+1, record.normal.y()+1, record.normal.z()+1),
+			0.5,
+		)
 	}
 
 	unitDirection := unitVector(r.direction())
-	t = 0.5 * (unitDirection.y() + 1.0)
+	t := 0.5 * (unitDirection.y() + 1.0)
+
 	return add(
 		scalarMul(newVec3From(1.0, 1.0, 1.0), 1.0-t),
 		scalarMul(newVec3From(0.5, 0.7, 1.0), t),
@@ -37,8 +26,7 @@ func color(r *ray) *vec3 {
 }
 
 func main() {
-	nx := 200
-	ny := 100
+	nx, ny := 200, 100
 
 	data := getPPMHeader(nx, ny)
 
@@ -46,6 +34,10 @@ func main() {
 	horizontal := newVec3From(4.0, 0.0, 0.0)
 	vertical := newVec3From(0.0, 2.0, 0.0)
 	origin := newVec3()
+
+	world := make(hitableList, 2)
+	world[0] = newSphereFrom(newVec3From(0, 0, -1.0), 0.5)
+	world[1] = newSphereFrom(newVec3From(0, -100.5, -1.0), 100)
 
 	for j := ny - 1; j >= 0; j-- {
 		for i := 0; i < nx; i++ {
@@ -64,7 +56,7 @@ func main() {
 				),
 			)
 
-			c := color(r)
+			c := color(r, world)
 
 			ir := int(255.99 * c.at(0))
 			ig := int(255.99 * c.at(1))
---
+++ sphere.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"math"
+)
+
+type sphere struct {
+	center *vec3
+	radius float64
+}
+
+func newSphere() *sphere {
+	return &sphere{
+		center: newVec3(),
+	}
+}
+
+func newSphereFrom(center *vec3, radius float64) *sphere {
+	return &sphere{
+		center: center,
+		radius: radius,
+	}
+}
+
+func (s *sphere) hit(r *ray, tMin, tMax float64, record *hitRecord) bool {
+	oc := sub(r.origin(), s.center)
+	a := dot(r.direction(), r.direction())
+	b := dot(oc, r.direction())
+	c := dot(oc, oc) - s.radius*s.radius
+
+	discriminant := b*b - a*c
+
+	if discriminant > 0 {
+		temp := (-b - math.Sqrt(discriminant)) / a
+
+		if tMin < temp && temp < tMax {
+			record.t = temp
+			record.p = r.pointAtParam(temp)
+			record.normal = scalarDiv(
+				sub(record.p, s.center),
+				s.radius,
+			)
+			return true
+		}
+
+		temp = (-b + math.Sqrt(discriminant)) / a
+
+		if tMin < temp && temp < tMax {
+			record.t = temp
+			record.p = r.pointAtParam(temp)
+			record.normal = scalarDiv(
+				sub(record.p, s.center),
+				s.radius,
+			)
+			return true
+		}
+
+	}
+
+	return false
+}
